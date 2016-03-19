72fd8cc4281720e213c583fc4655d3e348de72ff
Andreas Daniel Fried (VanillaCoding) <AndrolinRoot@googlemail.com>
2016-03-19 22:27:59 +0100

    Change position of mVuLogo;

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -32,7 +32,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     private final PhoneStatusBarView mView;
     private final float mIconAlphaWhenOpaque;
 
-    private View mLeftSide, mStatusIcons, mSignalCluster, mBattery, mClock, mNetworkTraffic; mVuLogo;
+    private View mLeftSide, mStatusIcons, mSignalCluster, mBattery, mClock, mVuLogo; mNetworkTraffic; 
     private Animator mCurrentAnimation;
 
     public PhoneStatusBarTransitions(PhoneStatusBarView view) {
