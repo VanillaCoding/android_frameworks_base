1c1f0613eafc7b9a254eee08f129129c4e55abdc
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-06-15 15:40:58 -0500

    SystemUI: detect rotation and resize mKeyguardBlur accordingly
    Change-Id: Icfd1d2de9929f9620332e3367885bec972063681

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3733,6 +3733,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mIconController.updateResources();
         mScreenPinningRequest.onConfigurationChanged();
         mNetworkController.onConfigurationChanged();
+        mStatusBarWindowManager.onConfigurationChanged(newConfig);
     }
 
     @Override
